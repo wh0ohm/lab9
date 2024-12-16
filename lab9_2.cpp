a97c664b72761d1bf03f601703406d5e9cdfeb79
wh0ohm <wararat.boonchieng@gmail.com>
2024-12-16 23:54:55 +0700

    lab9-2 complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int x,int y){
+	if(x <= 0 || y <= 0){
+		cout << "Invalid input" ;
+	}else{
+		for(int i=0 ; i<x ; i++){
+			for(int j=0 ; j<y ; j++){
+				cout << "O";
+			}
+			cout << "\n";
+		}
+	}
+}
 
 int main(){
 	
